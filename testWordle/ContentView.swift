15db6a8dcfc4cc2fda18bd9adc1a5330eaea9b42
Wesley Gore <jwesgore@gmail.com>
2024-03-27 15:04:56 -0500

    Initial Commit

---
+++ testWordle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testWordle
+//
+//  Created by Wesley Gore on 3/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testWordle/testWordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  testWordleApp.swift
+//  testWordle
+//
+//  Created by Wesley Gore on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct testWordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

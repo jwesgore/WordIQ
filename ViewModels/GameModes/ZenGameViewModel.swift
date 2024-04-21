527ce0dc693d4726a48986b098cb74f3fc084f2a
Wesley Gore <jwesgore@gmail.com>
2024-04-21 17:13:08 -0500

    change order of zen mode vm

--- ViewModels/GameModes/ZenGameViewModel.swift
+++ ViewModels/GameModes/ZenGameViewModel.swift
@@ -12,7 +12,20 @@ class ZenGameVM: WordGameVM, WordGameSubclassObserver, GameOverVMObserver {
         super.gameOverVM.addObserver(observer: self)
     }
     
-    // WordsColletions Functions
+    func addGameOverContents() {
+        gameOverVM.reset()
+        let numberOfGuesses = gameboardVM.currentPosition
+
+        gameOverVM.addResult(result: "You Win!")
+
+        let row1 = ["image": "pencil.line", "title": "Answer", "value": wordsCollection.selectedWord.word.uppercased()]
+        let row2 = ["image": "number.square", "title": "Guesses", "value": String(numberOfGuesses)]
+        
+        gameOverVM.addContentsRow(row: row1)
+        gameOverVM.addContentsRow(row: row2)
+    }
+    
+    // MARK: WordsColletions Functions
     // Check currentGuess for validity
     func submitGuess(_ guessWord: Word) {
         
@@ -29,28 +42,14 @@ class ZenGameVM: WordGameVM, WordGameSubclassObserver, GameOverVMObserver {
         }
     }
     
-    func addGameOverContents() {
-        gameOverVM.reset()
-        var numberOfGuesses = gameboardVM.currentPosition
-
-        gameOverVM.addResult(result: "You Win!")
-
-        let row1 = ["image": "pencil.line", "title": "Answer", "value": wordsCollection.selectedWord.word.uppercased()]
-        let row2 = ["image": "number.square", "title": "Guesses", "value": String(numberOfGuesses)]
-        
-        gameOverVM.addContentsRow(row: row1)
-        gameOverVM.addContentsRow(row: row2)
-    }
-    
-    /// GameOver Observer Function
-    /// Passes along which button was pressed in the GameOverView
+    // MARK: GameOver Observer Functions
+    // Passes along which button was pressed in the GameOverView
     func playAgain() {
         activeView = .standardgame
         gameOver()
         wordsCollection.updateSelectedWord()
     }
     
-    /// GameOver Observer Function
     func mainMenu() {
         activeView = ActiveView.tabview
     }

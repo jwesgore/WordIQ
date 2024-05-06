1e84117d34da5fa373a44cb475500ecc882e9e6a
Wesley Gore <jwesgore@gmail.com>
2024-05-06 16:36:54 -0500

    save changes before I fucking break everything

---
+++ Models/Save Models/RushSaveModel.swift
@@ -0,0 +1,34 @@
+import SwiftData
+
+@Model
+class RushSaveModel: BaseSaveModel {
+    // MARK: Standard Game Values
+    var totalGamesWon: Int
+    var bestGameScore: Int?
+    
+    // MARK: Protocol Values
+    var totalGamesPlayed: Int
+    var totalTimePlayed: Int
+    var totalXP: Int
+    var totalValidGuesses: Int
+    var totalInvalidGuesses: Int
+    var totalEasyDiff: Int
+    var totalMediumDiff: Int
+    var totalHardDiff: Int
+    
+    init(totalGamesWon: Int = 0, bestGameScore: Int, totalGamesPlayed: Int = 0, totalTimePlayed: Int = 0, totalXP: Int = 0, totalValidGuesses: Int = 0, totalInvalidGuesses: Int = 0, totalEasyDiff: Int = 0, totalMediumDiff: Int = 0, totalHardDiff: Int = 0) {
+        // MARK: Standard Game Values
+        self.totalGamesWon = totalGamesWon
+        self.bestGameScore = bestGameScore
+        
+        // MARK: Protocol Values
+        self.totalGamesPlayed = totalGamesPlayed
+        self.totalTimePlayed = totalTimePlayed
+        self.totalXP = totalXP
+        self.totalValidGuesses = totalValidGuesses
+        self.totalInvalidGuesses = totalInvalidGuesses
+        self.totalEasyDiff = totalEasyDiff
+        self.totalMediumDiff = totalMediumDiff
+        self.totalHardDiff = totalHardDiff
+    }
+}
---
+++ Models/Save Models/ZenSaveModel.swift
@@ -0,0 +1,32 @@
+import SwiftData
+
+@Model
+class ZenSaveModel: BaseSaveModel {
+    // MARK: Standard Game Values
+    var bestGameScore: Int?
+    
+    // MARK: Protocol Values
+    var totalGamesPlayed: Int
+    var totalTimePlayed: Int
+    var totalXP: Int
+    var totalValidGuesses: Int
+    var totalInvalidGuesses: Int
+    var totalEasyDiff: Int
+    var totalMediumDiff: Int
+    var totalHardDiff: Int
+    
+    init(bestGameScore: Int, totalGamesPlayed: Int = 0, totalTimePlayed: Int = 0, totalXP: Int = 0, totalValidGuesses: Int = 0, totalInvalidGuesses: Int = 0, totalEasyDiff: Int = 0, totalMediumDiff: Int = 0, totalHardDiff: Int = 0) {
+        // MARK: Standard Game Values
+        self.bestGameScore = bestGameScore
+        
+        // MARK: Protocol Values
+        self.totalGamesPlayed = totalGamesPlayed
+        self.totalTimePlayed = totalTimePlayed
+        self.totalXP = totalXP
+        self.totalValidGuesses = totalValidGuesses
+        self.totalInvalidGuesses = totalInvalidGuesses
+        self.totalEasyDiff = totalEasyDiff
+        self.totalMediumDiff = totalMediumDiff
+        self.totalHardDiff = totalHardDiff
+    }
+}
